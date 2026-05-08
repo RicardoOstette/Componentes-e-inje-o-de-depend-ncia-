3f79256687f1c52e5189628e57c5132611b3adc2
Ricardo Ostette <ostettericardo@gmail.com>
2026-05-08 23:24:37 +0200

    Criada implementação do programa na aplicacao

--- src/main/java/com/devsuperior/aula/AulaApplication.java
+++ src/main/java/com/devsuperior/aula/AulaApplication.java
@@ -1,13 +1,42 @@
 package com.devsuperior.aula;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class AulaApplication {
+import com.devsuperior.aula.entities.Order;
+import com.devsuperior.aula.services.OrderService;
 
+@SpringBootApplication
+public class AulaApplication implements CommandLineRunner{
+
+	@Autowired
+	private OrderService orderService;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(AulaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int code = sc.nextInt();
+		double basic = sc.nextDouble();
+		double discount = sc.nextDouble();
+		
+		Order order = new Order(code, basic, discount);
+		
+		System.out.println("Pedido código: " + code);
+		System.out.println("Valor total: R$" + String.format("%.2f", orderService.total(order)));
+		
+		sc.close();
 	}
 
 }
--- src/main/java/com/devsuperior/aula/services/OrderService.java
+++ src/main/java/com/devsuperior/aula/services/OrderService.java
@@ -1,9 +1,14 @@
 package com.devsuperior.aula.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.devsuperior.aula.entities.Order;
 
+@Service
 public class OrderService {
 	
+	@Autowired
 	private ShippingService shippingService;
 
 	public Double total(Order order) {
